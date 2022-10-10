29b57ae3c6119bf7043f3bccedbe4a61bf5a3390
Thuan Pham <phamtanthuan2311@gmail.com>
2022-10-10 14:04:31 +0700

    [👌Auto commit] more fixes

--- menudialog/app/src/main/java/vn/edu/stu/menudialog/MainActivity.java
+++ menudialog/app/src/main/java/vn/edu/stu/menudialog/MainActivity.java
@@ -2,7 +2,11 @@ package vn.edu.stu.menudialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
